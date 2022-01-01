864e29b72f83863ac3169f9acf21ce373e6f5545
Yuri Voitovich <voitovich200233@gmail.com>
2022-01-01 23:16:13 +0300

    SwitchChange

--- src/Lesson1/Main.java
+++ src/Lesson1/Main.java
@@ -1,6 +1,6 @@
 package Lesson1;
 
-import com.sun.source.doctree.SystemPropertyTree;
+//import com.sun.source.doctree.SystemPropertyTree;
 
 import java.util.Scanner;
 
@@ -16,138 +16,110 @@ public class Main {
             System.out.println("Part 3 - Cycles");
             int LessonPartNumber = scanner.nextInt();
             switch (LessonPartNumber) {
-                case 0 -> {
+                case 0: {
                     isRunning = false;
                 }
-                case 1 -> {
+                case 1: {
                     System.out.println("Lesson 1: Part " + LessonPartNumber + " Please, enter task number (0 - exit)");
                     int taskNumber = scanner.nextInt();
                     switch (taskNumber) {
-                        case 0 -> {
-                            isRunning = false;
-                            break;
-                        }
+                        case 0 -> isRunning = false;
                         case 1 -> {
                             System.out.println("Task 1:");
                             Lesson1Tasks.Part1.task1();
-                            break;
                         }
                         case 2 -> {
                             System.out.println("Task 2:");
                             Lesson1Tasks.Part1.task2();
-                            break;
                         }
                         case 3 -> {
                             System.out.println("Task 3:");
                             Lesson1Tasks.Part1.task3();
-                            break;
                         }
                         case 4 -> {
                             System.out.println("Task 4:");
                             Lesson1Tasks.Part1.task4();
-                            break;
                         }
                         case 5 -> {
                             System.out.println("Task 5:");
                             Lesson1Tasks.Part1.task5();
-                            break;
                         }
                         case 6 -> {
                             System.out.println("Task 6:");
                             Lesson1Tasks.Part1.task6();
-                            break;
                         }
                         default -> System.out.println("Incorrect number, please, try again");
                     }
                 }
-                case 2 -> {
+                case 2: {
                     System.out.println("Lesson 1: Part " + LessonPartNumber + " Please, enter task number (0 - exit)");
                     int taskNumber = scanner.nextInt();
                     switch (taskNumber) {
-                        case 0 -> {
-                            isRunning = false;
-                            break;
-                        }
+                        case 0 -> isRunning = false;
                         case 1 -> {
                             System.out.println("Task 1:");
                             Lesson1Tasks.Part2.task1();
-                            break;
                         }
                         case 2 -> {
                             System.out.println("Task 2:");
                             Lesson1Tasks.Part2.task2();
-                            break;
                         }
                         case 3 -> {
                             System.out.println("Task 3:");
                             Lesson1Tasks.Part2.task3();
-                            break;
                         }
                         case 4 -> {
                             System.out.println("Task 4:");
                             Lesson1Tasks.Part2.task4();
-                            break;
                         }
                         case 5 -> {
                             System.out.println("Task 5:");
                             Lesson1Tasks.Part2.task5();
-                            break;
                         }
                         default -> System.out.println("Incorrect number, please, try again");
                     }
                 }
-                case 3 -> {
+                case 3: {
                     System.out.println("Lesson 1: Part " + LessonPartNumber + " Please, enter task number (0 - exit)");
                     int taskNumber = scanner.nextInt();
                     switch (taskNumber) {
-                        case 0 -> {
-                            isRunning = false;
-                            break;
-                        }
+                        case 0 -> isRunning = false;
                         case 1 -> {
                             System.out.println("Task 1:");
                             Lesson1Tasks.Part3.task1();
-                            break;
                         }
                         case 2 -> {
                             System.out.println("Task 2:");
                             Lesson1Tasks.Part3.task2();
-                            break;
                         }
                         case 3 -> {
                             System.out.println("Task 3:");
                             Lesson1Tasks.Part3.task3();
-                            break;
                         }
                         case 4 -> {
                             System.out.println("Task 4:");
                             Lesson1Tasks.Part3.task4();
-                            break;
                         }
                         case 5 -> {
                             System.out.println("Task 5:");
                             Lesson1Tasks.Part3.task5();
-                            break;
                         }
                         case 6 -> {
                             System.out.println("Task 6:");
                             Lesson1Tasks.Part3.task6();
-                            break;
                         }
                         case 7 -> {
                             System.out.println("Task 7:");
                             Lesson1Tasks.Part3.task7();
-                            break;
                         }
                         case 8 -> {
                             System.out.println("Task 8:");
                             Lesson1Tasks.Part3.task8();
-                            break;
                         }
                         default -> System.out.println("Incorrect number, please, try again");
                     }
                 }
-                default -> System.out.println("Please, Enter correct lesson part number");
+                default: System.out.println("Please, Enter correct lesson part number");
             }
 
 
