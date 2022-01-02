3fc970dfa173cd4b83684367e836b67f8d6cc759
Yuri Voitovich <voitovich200233@gmail.com>
2022-01-02 22:04:58 +0300

    added some tasks lesson 3.StringAndStringBuilder

---
+++ src/Lesson3/StringAndStringBuilder/Task1/Main.java
@@ -0,0 +1,26 @@
+package Lesson3.StringAndStringBuilder.Task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int maximumSpacesCount(String str) {
+        int maximumSpacesCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int spacesCount = 0;
+            if (str.charAt(i) == ' ') {
+                for (int j = i; (j < str.length()) && (str.charAt(j) == ' '); j++) {
+                    spacesCount++;
+                }
+                if (spacesCount > maximumSpacesCount) {
+                    maximumSpacesCount = spacesCount;
+                }
+            }
+        }
+        return maximumSpacesCount;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println("Maximum spaces count = " + maximumSpacesCount(str));
+    }
+}
---
+++ src/Lesson3/StringAsCharacterArray/Task1/Main.java
@@ -0,0 +1,13 @@
+package Lesson3.StringAsCharacterArray.Task1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int stringSize = scanner.nextInt();
+        char[] string = new char[stringSize];
+
+    }
+}
